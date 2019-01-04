a1e9f86893c8466acb509b946f4f5f3676f11c09
Buddy <buddyy93@gmail.com>
2019-01-04 13:35:52 +0800

    initialized repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const hbs = require("hbs");
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.set("view-engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {});
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    message: "Hello World"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
