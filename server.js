9e19b2e07a0fc804974eda6327d38abf0027b6b3
Buddy <buddyy93@gmail.com>
2019-01-04 13:42:11 +0800

    remove middleware

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var app = express();
 app.set("view-engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
-app.use((req, res, next) => {});
-
 app.get("/", (req, res) => {
   res.render("home.hbs", {
     message: "Hello World"
